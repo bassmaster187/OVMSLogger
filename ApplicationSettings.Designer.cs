984450e4fe8c45e24f9a67880f0ff0c0ade9a2ae
Christian P. <mail@pogea.de>
2026-03-04 23:15:40 +0100

    Pre-upgrade commit: saving current state before .NET 10.0 upgrade

--- ApplicationSettings.Designer.cs
+++ ApplicationSettings.Designer.cs
@@ -12,7 +12,7 @@ namespace OVMS {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class ApplicationSettings : global::System.Configuration.ApplicationSettingsBase {
         
         private static ApplicationSettings defaultInstance = ((ApplicationSettings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new ApplicationSettings())));
